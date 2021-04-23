cb26c8d4009b989d3886e6272d0bf41667bc3bd9
Vilena Taraskina <vilena_taraskina@mail.ru>
2021-04-23 12:42:04 +0200

    Javascript

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+console.log ("funguji!");
+
+let currentSign="circleHraje";
+const kdoHrajeElm=document.querySelector ("#hrac");
+
+const currentPlayer = (event) => {
+if(currentSign==="circleHraje") {
+event.target.classList.add("buttons__buttonTlacitko--circleHraje");
+event.target.disabled = true;
+kdoHrajeElm.src = "images/cross.svg";
+kdoHrajeElm.alt = "obrazek krize";
+currentSign = "krizek";
+} else {
+event.target.classList.add("buttons__buttonTlacitko--krizek");
+event.target.disabled = true;
+kdoHrajeElm.src="images/circle.svg";
+kdoHrajeElm.alt = "obrazek kolecka";
+currentSign = "circleHraje";
+}
+};
+
+const buttonsElm = document.querySelectorAll(".buttonTlacitko");
+for (let i = 0; i < buttonsElm.length; i++) {
+  buttonsElm[i].addEventListener('click', currentPlayer);
+}
+
